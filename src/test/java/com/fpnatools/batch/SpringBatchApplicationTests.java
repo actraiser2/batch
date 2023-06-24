0a3c002bd13f5fe98141b13706c96244fb670b18
jose miguel <josemiguel.besada@finleap.com>
2023-06-24 10:40:33 +0200

    added tests

--- src/test/java/com/fpnatools/batch/SpringBatchApplicationTests.java
+++ src/test/java/com/fpnatools/batch/SpringBatchApplicationTests.java
@@ -2,8 +2,11 @@ package com.fpnatools.batch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/application-test.yaml")
 class SpringBatchApplicationTests {
 
 	@Test
