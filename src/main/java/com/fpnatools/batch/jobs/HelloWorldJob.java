0405d5b3f3ae4d47aecb64ab2239555981e469fa
jose miguel <josemiguel.besada@finleap.com>
2023-06-24 14:45:25 +0200

    improved the tests

--- src/main/java/com/fpnatools/batch/jobs/HelloWorldJob.java
+++ src/main/java/com/fpnatools/batch/jobs/HelloWorldJob.java
@@ -1,15 +1,18 @@
 package com.fpnatools.batch.jobs;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
@@ -21,16 +24,25 @@ public class HelloWorldJob {
 	private StepBuilderFactory stepBuilderFactory;
 	
 	@Bean
-	public Step step1() {
-		return stepBuilderFactory.get("step1").tasklet((stepContr, chunckContext) -> {
-			log.info("Executing step1");
-			return RepeatStatus.FINISHED;
-		}).build();
+	public Step helloWorlStep() {
+		return stepBuilderFactory.get("step1").tasklet(helloWorldTaskLet(null)).build();
 	}
 	
 	
 	@Bean
-	public Job job(Step step1) {
-		return jobBuilderFactory.get("job-helloWorld").start(step1).build();
+	public Job job() {
+		return jobBuilderFactory.get("helloWorld").start(helloWorlStep()).
+				build();
 	}
+	
+	@Bean
+	@StepScope()
+	public Tasklet helloWorldTaskLet(@Value("#{jobParameters['startDate']}") String startDate){
+		return (stepContr, chunkContext) -> {
+				log.info("Executing taskelet wirh date startDate:" + startDate);
+				return RepeatStatus.FINISHED;
+			};
+	}
+	
+	
 }
--- src/test/java/com/fpnatools/batch/SpringBatchApplicationTests.java
+++ src/test/java/com/fpnatools/batch/SpringBatchApplicationTests.java
@@ -1,16 +1,41 @@
 package com.fpnatools.batch;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
-@TestPropertySource(locations = "classpath:/application-test.yaml")
+@TestPropertySource(locations = "classpath:/application-test.properties")
+@Slf4j
 class SpringBatchApplicationTests {
 
+	@Autowired JobLauncher jobLauncher;
+	@Autowired Job job;
+	@Autowired Environment env;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+		var jobParameters = new JobParametersBuilder().addString("startDate", 
+				LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))).
+			toJobParameters();
 		
+		jobLauncher.run(job, jobParameters);
+		
+		log.info("Env:" + env.getProperty("spring.datasource.url"));
 	} 
 
 }
